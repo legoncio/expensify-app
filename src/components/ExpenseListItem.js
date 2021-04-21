d1155f999c5ebc1e287b03ac51a8eb1b12b0d157
Leon Gonzalez <leonesteban@gmail.com>
2021-04-21 17:02:40 -0500

    Setup formatting for ammount and created at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { DateTime } from 'luxon'
+import numeral from 'numeral'
 
 const ExpenseListItem = (props) => (
     <div>
@@ -7,7 +9,11 @@ const ExpenseListItem = (props) => (
             <Link to={`/edit/${props.id}`}>
                 <h3>{props.description}</h3>
             </Link>
-            <p>Amount: {props.amount} - Created at: {props.createdAt}</p>
+            <p>
+            Amount: {numeral(props.amount / 100).format('$0,0.00')}
+            -
+            Created at: {DateTime.fromMillis(props.createdAt).toLocaleString(DateTime.DATE_FULL)}
+            </p>
         </li>
     </div>
 )
